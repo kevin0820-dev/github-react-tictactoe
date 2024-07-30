f8def1a4fd23aa6fbb65b2c4eb17ac0cf657ebe7
kevin0820-dev <kevinallen5877066@gmail.com>
2024-07-30 07:59:01 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 let player = 0;
 
-function Square(value, onSquareClick) {
+function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -13,11 +13,10 @@ function Square(value, onSquareClick) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick(index) {
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[index] = player % 2 == 0 ? "X" : "o";
+    nextSquares[i] = player % 2 == 0 ? "X" : "O";
     setSquares(nextSquares);
-    checkWin();
     player++;
   }
 
@@ -25,23 +24,19 @@ export default function Board() {
     <>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[0]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[0]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[0]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[0]} onSquareClick={() => handleClick(5)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[0]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[0]} onSquareClick={() => handleClick(8)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
-}
-
-function checkWin() {
-  console.log("i cant check who win");
 }
